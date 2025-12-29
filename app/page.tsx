4e0e2795418c729db0215916453277ac2906eb09
mathyx3 <matheussaraiva6969@gmail.com>
2025-12-29 00:08:30 -0300

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import Header from '@/components/Header'
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <main>
+        <h1>Geek Collection</h1>
+        <p>Seu banco de dados geek</p>
+      </main>
+    </>
+  )
+}
